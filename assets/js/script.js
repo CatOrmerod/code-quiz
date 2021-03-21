2ebee585f5bcbb4f523444d45148f31337282668
Cat Ormerod <catormerod@gmail.com>
2021-03-21 12:10:31 +1100

    further work into the question function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,51 @@
 // questions variable
-var questions [
+var quizQuestions = [
     { 
-        question: "Question 1 Question",
-        choices: ["A answer", "B answer", "C answer", "D answer"],
-        answer: "Question 1 Answer"
+        question: "Inside which HTML element do we put the JavaScript?",
+        choices: ["<javascript>", "<script>", "<body>", "<js>"],
+        answer: "<script>"
     },
     { 
-        question: "Question 2 Question",
-        choices: ["A answer", "B answer", "C answer", "D answer"],
-        answer: "Question 2 Answer"
+        question: "What does CSS stand for?",
+        choices: ["Computer Style Sheets", "Cascading Computer Sheets", "Creative Styling Sheets", "Cascading Style Sheets"],
+        answer: "Cascading Style Sheets"
     },
     { 
-        question: "Question 3 Question",
-        choices: ["A answer", "B answer", "C answer", "D answer"],
-        answer: "Question  Answer"
+        question: "Choose the correct HTML element for the largest heading:",
+        choices: ["<h1>", "<heading>", "<h6>", "<head>"],
+        answer: "<h1>"
     },
     { 
-        question: "Question 4 Question",
-        choices: ["A answer", "B answer", "C answer", "D answer"],
-        answer: "Question 4 Answer"
+        question: "How to write an IF statement in JavaScript?",
+        choices: ["if i = 5", "if(i == 5)", "if i = 5 then", "if (i == 5) then"],
+        answer: "if(i == 5)"
     },
     { 
-        question: "Question 5 Question",
-        choices: ["A answer", "B answer", "C answer", "D answer"],
-        answer: "Question 5 Answer"
+        question: "Which CSS property controls the text size?",
+        choices: ["text-style", "font-style", "text-size", "font-size"],
+        answer: "font-size"
     },
     { 
-        question: "Question 6 Question",
-        choices: ["A answer", "B answer", "C answer", "D answer"],
-        answer: "Question 6 Answer"
-    },
-]
+        question: "How can you make a numbered list?",
+        choices: ["<ol>", "<list>", "<li>", "<ul>"],
+        answer: "<ol>"
+    }]
 
 // other variables
 
-var questionsRandom = "";
+var totalQuestions = quizQuestions.length;
+var currentQuestions = 0;
 
 var timer = 60;
 var deducter = 0;
 var interval
+var losses = 0;
 
-document.getElementById("start").addEventListener("click, startGame");
+document.getElementById("start").addEventListener("click", startGame);
 
 function startGame () {
     startTimer();
-    
+    loadQuestion ();
 }
 
 function startTimer () {
@@ -52,11 +53,42 @@ function startTimer () {
     timer = 60;
     interval = setInterval(function () {
         document.getElementById("time-remaining").innerHTML = timer
-        if (timer === 0 {
+        if (timer === 0) {
             alert("You have lost this round")
+            losses++
             clearInterval (interval);
             interval = undefined
+            document.getElementById("losses").innerHTML = losses
+            document.getElementById("time-deducted").innerHTML = losses*10
         }
         timer--
     }, 1000)
+}
+
+function loadQuestion () {
+    if (currentQuestions === totalQuestions) {
+        return finalScore ();
+    }
+
+    for (var i =0; i < totalQuestions; i++) {
+    var currentQuestion = quizQuestions[currentQuestions];
+    document.getElementById("question").innerHTML = currentQuestion.question[0];
+    document.getElementById("choice-1").innerHTML = currentQuestion.choices[0];
+    document.getElementById("choice-2").innerHTML = currentQuestion.choices[1];
+    document.getElementById("choice-3").innerHTML = currentQuestion.choices[2];
+    document.getElementById("choice-4").innerHTML = currentQuestion.choices[3];
+
+        
+    console.log(currentQuestion);
+    console.log(currentQuestion.choices[2]);
+    }
+
+
+
+}
+
+
+
+function finalScore () {
+
 }
